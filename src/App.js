15f54739b6afbe96e36495c35ce62c374c5003da
tamagosann <hssvs619@gmail.com>
2022-09-15 23:11:50 +0900

    Replace plain three.js to r3f

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
-import { WebGLRenderer } from "three";
-import { BoxGeometry } from "three";
-import { Mesh } from "three";
-import { MeshBasicMaterial } from "three";
-import { PerspectiveCamera } from "three";
-import { Scene } from "three";
-import logo from "./logo.svg";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const Box = () => {
+  const ref = useRef();
+  useFrame((state) => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="blue" />
+    </mesh>
+  );
+};
 
 function App() {
-  const scene = new Scene();
-  const camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Canvas style={{ background: "black" }}>
+        <Box />
+      </Canvas>
+    </div>
   );
-
-  const renderer = new WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.innerHTML = "";
-  document.body.appendChild(renderer.domElement);
-
-  const geometry = new BoxGeometry();
-  const material = new MeshBasicMaterial({
-    color: "blue",
-  });
-  camera.position.z = 5;
-  const cube = new Mesh(geometry, material);
-  scene.add(cube);
-
-  function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-  }
-
-  animate();
-
-  window.addEventListener("resize", () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-  });
-
-  return null;
 }
 
 export default App;
