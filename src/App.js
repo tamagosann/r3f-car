fe2b115c429bfbcf55e1213386520209207612cf
tamagosann <hssvs619@gmail.com>
2022-09-15 23:05:12 +0900

    Add cube with plain three.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { WebGLRenderer } from "three";
+import { BoxGeometry } from "three";
+import { Mesh } from "three";
+import { MeshBasicMaterial } from "three";
+import { PerspectiveCamera } from "three";
+import { Scene } from "three";
+import logo from "./logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
   );
+
+  const renderer = new WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.innerHTML = "";
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new BoxGeometry();
+  const material = new MeshBasicMaterial({
+    color: "blue",
+  });
+  camera.position.z = 5;
+  const cube = new Mesh(geometry, material);
+  scene.add(cube);
+
+  function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+
+  animate();
+
+  window.addEventListener("resize", () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  });
+
+  return null;
 }
 
 export default App;
